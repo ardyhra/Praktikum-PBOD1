3082aac42ef5337665b64eb208f158738dfdee5f
ardyhra <145884823+ardyhra@users.noreply.github.com>
2024-03-03 00:27:21 +0700

    Update Course.java

--- Pertemuan 2/TugasTambahan/Course.java
+++ Pertemuan 2/TugasTambahan/Course.java
@@ -8,7 +8,6 @@ public class Course {
     public List<Student> studentsEnrolled;
 
     Course(String courseCode, String courseName, Lecture lecture) {
-        // TODO: buatlah fungsi konstruktor
         this.studentsEnrolled = new ArrayList<>();
         this.courseCode = courseCode;
         this.courseName = courseName;
@@ -32,7 +31,6 @@ public class Course {
     }
 
     public void addStudent(Student student) {
-        // TODO: buatlah fungsi untuk menambah seorang mahasiswa
         this.studentsEnrolled.add(student);
         if (!student.coursesEnrolled.contains(this)) {
             student.enrollInCourse(this);
@@ -40,21 +38,16 @@ public class Course {
     }
 
     public void removeStudent(Student student) {
-        // TODO: buatlah fungsi untuk menghapus seorang mahasiswa
         int index = this.studentsEnrolled.indexOf(student);
         if (index != -1) {
             this.studentsEnrolled.remove(index);
-        } else {
-            System.out.println("Mahasiswa tidak ditemukan dalam daftar mahasiswa kursus ini");
+            student.removeCourse(this);
         }
+        
     }
 
     public void viewEnrolledStudents() {
-        /* TODO: buatlah fungsi untuk menampilkan seluruh 
-           student yang mengambil course. 
 
-           Hint: gunakan loop dan method getDetails dari Student
-        */
         System.out.printf("Daftar mahasiswa yang mengikuti course %s : \n",getCourseName());
         for (Student s : studentsEnrolled) {
             s.getDetails();
@@ -63,11 +56,9 @@ public class Course {
     }
 
     public void getDetails() {
-        /* TODO: buat fungsi untuk print detail dari Course, 
-           menampilkan kode course, nama course, dan dosen pengampu
-        */ 
+
         System.out.printf("Kode Kursus : %s, Nama Kursus : %s, Dosen Pengampu : %s\n", getCourseCode(), getCourseName(), this.lecture.getName());
     }      
 
-    // Other methods...
+
 }
