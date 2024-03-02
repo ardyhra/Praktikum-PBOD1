8f8aea6e870b87a12a59013db654b682c62ac8be
ardyhra <145884823+ardyhra@users.noreply.github.com>
2024-03-03 00:28:04 +0700

    Update Lecture.java

--- Pertemuan 2/TugasTambahan/Lecture.java
+++ Pertemuan 2/TugasTambahan/Lecture.java
@@ -2,9 +2,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Lecture {
-    /* TODO: implementasi enkapsulasi dengan 
-       berikan akses yang sesuai
-    */
 
     private String name;
     private int age;
@@ -13,7 +10,6 @@ public class Lecture {
     public List<Course> coursesTaught;
 
     Lecture(String name, int age, String address, int employeeID) {
-        // TODO: buatlah fungsi konstruktor
         this.coursesTaught = new ArrayList<>();
         setName(name);
         setAge(age);
@@ -53,27 +49,16 @@ public class Lecture {
             System.out.println("ID tidak valid");
     }
     public void getDetails() {
-        /* TODO: buat fungsi untuk print detail dari Lecture,
-           menampilkan nama, umur, alamat, dan ID
-        */
-        System.out.printf("Nama : %s\n",getName());
-        System.out.printf("Umur : %d tahun\n",getAge());
-        System.out.printf("Alamat : %s\n",getAddress());
-        System.out.printf("ID Karyawan : %d\n",getEmployeeID());
+        System.out.printf("Nama : %s, Umur : %d, Alamat : %s, ID Karyawan : %d\n",getName(),getAge(),getAddress(),getEmployeeID());
     }    
 
     public void teachCourse(Course course) {
-        // TODO: buatlah fungsi untuk menambah sebuah course 
         this.coursesTaught.add(course);
         course.setLecture(this);
     }
 
     public void viewTaughtCourses() {
-        /* TODO: buatlah fungsi untuk menampilkan seluruh course
-           yang diampu oleh dosen.
 
-           Hint: gunakan loop dan method getDetails dari Course
-        */
         System.out.printf("Course yang diampu dosen %s:\n", getName());
         for (Course c : coursesTaught) {
             c.getDetails();
@@ -81,5 +66,4 @@ public class Lecture {
         System.out.println();
     }
 
-    // Other methods...
 }
