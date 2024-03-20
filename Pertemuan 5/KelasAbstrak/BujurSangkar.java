87a31d3094a059c8062d55d48e66ee399032b894
ardyhra <145884823+ardyhra@users.noreply.github.com>
2024-03-20 11:01:36 +0700

    Update BujurSangkar.java

--- Pertemuan 5/KelasAbstrak/BujurSangkar.java
+++ Pertemuan 5/KelasAbstrak/BujurSangkar.java
@@ -9,6 +9,7 @@ public class BujurSangkar extends BangunDatar{
 /*apa yang terjadi apabila kelas BujurSangkar tidak membuat implementasi metode abstrak yang ada pada kelas BangunDatar? jelaskan !
 Jawab : akan muncul error karena metode abstrak hitungLuas(double sisi) dalam kelas abstrak bangun datar belum diimplementasikan 
 dalam kelas BujurSangkar yang merupakan turunan dari kelas BangunDatar. Hal ini terjadi karena syarat dari kelas anak yang diturunkan dari 
-kelas abstrak adalah harus mengimplementasikan semua metode abstrak dari kelas parentnya.
+kelas abstrak adalah harus mengimplementasikan semua metode abstrak dari kelas parentnya, jika tidak maka kelas anak juga harus menjadi kelas
+abstrak.
 
 */
